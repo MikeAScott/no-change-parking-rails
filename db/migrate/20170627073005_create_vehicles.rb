3e2d4905ddd5727268eb0af8273743cc44c38a62
Mike Scott <mike.scott@magentys.io>
2017-08-02 18:35:55 +0100

    Remove vehicle db table

--- db/migrate/20170627073005_create_vehicles.rb
+++
@@ -1,9 +0,0 @@
-class CreateVehicles < ActiveRecord::Migration[5.0]
-  def change
-    create_table :vehicles do |t|
-      t.string :vrn
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,4 @@ ActiveRecord::Schema.define(version: 20170627073005) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "vehicles", force: :cascade do |t|
-    t.string   "vrn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
